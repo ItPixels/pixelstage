df93cbe01c313bf5152f3030ee1057d6d7e6d4f5
ItPixels <murtazin456@gmail.com>
2026-01-09 10:05:39 +0400

    Fix Clerk middleware for Vercel

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,22 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/api/webhooks(.*)"],
-  ignoredRoutes: ["/api/webhooks(.*)"],
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth().protect();
+  }
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|ico|png|jpg|jpeg|gif|svg|ttf|woff|woff2)).*)",
+    "/((?!.*\\..*|_next).*)",
+    "/",
     "/(api|trpc)(.*)",
   ],
 };
